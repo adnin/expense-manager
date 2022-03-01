57150e6f864024587af95e4b9796126d4b8246bb
Adnin Onofre <adninonofre@gmail.com>
2022-03-01 13:21:07 +0800

    add login layout

--- client/tests/unit/login.spec.js
+++ client/tests/unit/login.spec.js
@@ -13,6 +13,22 @@ describe('Login Page', () => {
             const msg = 'Login';
             expect(wrapper.find('h1').text()).toMatch(msg);
         });
+
+        it('has email input', () => {
+            const email = wrapper.find('[data-email]');
+            expect(email.exists()).toBe(true);
+            expect(email.element.type).toEqual('email');
+        });
+
+        it('has password input', () => {
+            const password = wrapper.find('[data-password]');
+            expect(password.exists()).toBe(true);
+            expect(password.element.type).toEqual('password');
+        });
+
+        it('has login button', () => {
+            expect(wrapper.find('[data-login]').exists()).toBe(true);
+        });
     });
 
     describe('Interaction', () => {});
